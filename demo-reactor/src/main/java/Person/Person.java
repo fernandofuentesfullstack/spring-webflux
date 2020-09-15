7e9eefa4778c2a61803491cffd99e58dcf012440
fernandofuentesfullstack <fernandofuentesfullstack@gmail.com>
2020-09-15 19:53:53 +0200

    added Person and main with reactor and rxjava2

---
+++ demo-reactor/src/main/java/Person/Person.java
@@ -0,0 +1,11 @@
+package Person;
+
+import lombok.*;
+
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @ToString
+public class Person {
+
+    private Integer id;
+    private String name;
+    private Integer age;
+}
--- demo-reactor/src/main/java/com/fernandofuentesfullstack/demoreactor/DemoReactorApplication.java
+++ demo-reactor/src/main/java/com/fernandofuentesfullstack/demoreactor/DemoReactorApplication.java
@@ -1,15 +1,28 @@
 package com.fernandofuentesfullstack.demoreactor;
 
+import Person.Person;
+import io.reactivex.Observable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 public class DemoReactorApplication implements CommandLineRunner {
 
-	private static final Logger Log = LoggerFactory.getLogger(DemoReactorApplication.class);
+	private static final Logger log = LoggerFactory.getLogger(DemoReactorApplication.class);
+
+	public void reactor() {
+		Mono.just(new Person(1, "Fernando Fuentes", 30))
+				.subscribe(p -> log.info("[Reactor] Person: " + p ));
+	}
+
+	public void rxjava2() {
+		Observable.just(new Person(1, "Fernando Fuentes", 30))
+				.subscribe(p -> log.info("[RxJava2] Person: " + p ));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoReactorApplication.class, args);
@@ -17,6 +30,7 @@ public class DemoReactorApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		
+		reactor();
+		rxjava2();
 	}
 }
